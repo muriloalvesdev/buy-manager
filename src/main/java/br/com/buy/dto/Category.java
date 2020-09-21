8d5090d779e0f2dc0f28a60e1b55111ea45e7338
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-21 17:01:10 -0300

    create DataTransferObjects

---
+++ src/main/java/br/com/buy/dto/Category.java
@@ -0,0 +1,9 @@
+package br.com.buy.dto;
+
+import lombok.Data;
+
+@Data
+public class Category {
+  private int id;
+  private String category;
+}
---
+++ src/main/java/br/com/buy/dto/Food.java
@@ -0,0 +1,27 @@
+package br.com.buy.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class Food {
+
+  @JsonProperty("attributes")
+  private String attributes;
+
+  @JsonProperty("base_qty")
+  private int baseQuantity;
+
+  @JsonProperty("base_unit")
+  private String baseUnit;
+
+  @JsonProperty("category_id")
+  private int categoryId;
+
+  @JsonProperty("description")
+  private String description;
+
+  @JsonProperty("id")
+  private int id;
+
+}
