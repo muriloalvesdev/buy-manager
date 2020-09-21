94152b41143afb73f820848e006b148ba64cd87e
Murilo Alves <muriloalvesdev.ti@gmail.com>
2020-09-21 16:31:45 -0300

    init project

---
+++ src/main/java/br/com/buy/BuyManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.buy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/buy/BuyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.buy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
