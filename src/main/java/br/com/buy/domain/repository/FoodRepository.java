71d3152b4cafbbfbc83eb1effa56900ea83f8013
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-22 21:48:14 -0300

    impl methods in foodRepository

--- src/main/java/br/com/buy/domain/repository/FoodRepository.java
+++ src/main/java/br/com/buy/domain/repository/FoodRepository.java
@@ -1,8 +1,14 @@
 package br.com.buy.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.buy.domain.entity.Cart;
 import br.com.buy.domain.entity.Food;
 
 public interface FoodRepository extends JpaRepository<Food, UUID> {
+  Optional<Food> findByExternalId(String externalId);
+
+  List<Food> findByCart(Cart cart);
 }
