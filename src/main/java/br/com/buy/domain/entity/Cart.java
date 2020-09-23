174893e8795232238e87c87e618446c12ce5f4b9
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-22 21:47:57 -0300

    improving the relationship of entities

--- src/main/java/br/com/buy/domain/entity/Cart.java
+++ src/main/java/br/com/buy/domain/entity/Cart.java
@@ -1,30 +1,31 @@
 package br.com.buy.domain.entity;
 
-import java.util.List;
 import java.util.UUID;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(builderMethodName = "newBuilder")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
-@Table(name = "food")
+@Getter
+@Setter
+@Table(name = "cart")
 public class Cart extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private UUID uuid;
 
-  @OneToMany(mappedBy = "cart", fetch = FetchType.LAZY, orphanRemoval = true)
-  private List<Food> foods;
+  private int total;
+
 }
--- src/main/java/br/com/buy/domain/entity/Food.java
+++ src/main/java/br/com/buy/domain/entity/Food.java
@@ -1,29 +1,32 @@
 package br.com.buy.domain.entity;
 
+import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(builderMethodName = "newBuilder")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @TypeDef(name = "jsonb", typeClass = JsonNodeBinaryType.class)
+@Getter
+@Setter
 @Entity
 @Table(name = "food")
 public class Food extends BaseEntity {
@@ -32,12 +35,8 @@ public class Food extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private UUID uuid;
 
-  @Column(name = "external_id")
+  @Column(name = "external_id", unique = true)
   private int externalId;
-
-  @Type(type = "jsonb")
-  @Column(columnDefinition = "metadata")
-  private JsonNode attributes;
 
   @Column(name = "base_qty")
   private int baseQuantity;
@@ -49,20 +48,12 @@ public class Food extends BaseEntity {
   private int categoryId;
 
   @Column
-  private String description;
+  private String name;
 
-  @Column(name = "rae")
-  private String retinolActivityEquivalent;
+  private int count;
 
-  @Column(name = "re")
-  private String retinolEquivalent;
-
-  @Column(name = "vitamin_c")
-  private String vitamin;
-
-  @ManyToOne(fetch = FetchType.EAGER)
-  @JoinColumn(name = "cart_uuid", referencedColumnName = "uuid",
-      foreignKey = @ForeignKey(name = "uuid"))
-  private Cart cart;
+  @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+  @JoinTable(name = "food_cart")
+  private List<Cart> cart;
 
 }
