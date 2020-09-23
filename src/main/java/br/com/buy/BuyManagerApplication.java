3fb912cad6f8c4b39019db7480f4281433b5f994
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-22 23:48:42 -0300

    checkstyle

--- src/main/java/br/com/buy/BuyManagerApplication.java
+++ src/main/java/br/com/buy/BuyManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BuyManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BuyManagerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BuyManagerApplication.class, args);
+  }
 
 }
