1b81d58055caec3fbc2a6c3ee57c7cdd5a747b5d
Murilo Alves <muriloalvesdev.ti@gmail.com>
2020-10-28 17:00:55 -0300

    Update script.sh

--- .ci/script.sh
+++ .ci/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  export MAVEN_OPTS="-Xmx1G -Xms128m"
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=muriloalvesdev_buy-manager
+mvn clean install -Ptest
